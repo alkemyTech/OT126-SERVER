ceaad8a583933d5ffb75395dedd4d91f427a65bb
Eduardo Corgniali <educorgniali@gmail.com>
2022-01-21 18:00:39 -0300

    added field validation functionality create activities

---
+++ middlewares/activities-validations.js
@@ -0,0 +1,26 @@
+const { body } = require('express-validator');
+const { executeValidation } = require('./validation-index');
+
+const create = [
+    body('name')
+        .exists().withMessage('required parameter')
+        .isString().withMessage('must be a string')
+        .notEmpty().withMessage('cannot be empty')
+    ,
+    body('content')
+        .exists().withMessage('required parameter')
+        .isString().withMessage('must be a string')
+        .notEmpty().withMessage('cannot be empty')
+    ,
+    body('image')
+        .exists().withMessage('required parameter')
+        .isString().withMessage('must be a string')
+        .notEmpty().withMessage('cannot be empty')
+    ,
+    executeValidation
+]
+
+
+module.exports = {
+    create
+}
