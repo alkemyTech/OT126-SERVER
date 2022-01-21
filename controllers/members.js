0a2ba59106943aeb919ed20d1853a9b040cdcf94
Manu Ominuka <ominuka.mauro@gmail.com>
2022-01-21 12:07:14 -0300

    Add controller for getAll members

--- controllers/members.js
+++ controllers/members.js
@@ -9,6 +9,15 @@ const remove = async (req, res, next) => {
   }
 };
 
+const getAll= async(req,res,next)=>{
+  try {
+    const data = await membersService.getAll()
+    res.status(200).json({data})
+  } catch (error) {
+    next(error);
+  }
+}
 module.exports = {
-  remove
+  remove,
+  getAll
 };
