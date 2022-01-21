c2397c99de32c996ba559122ec76cb1a34f55ee0
Manu Ominuka <ominuka.mauro@gmail.com>
2022-01-21 12:09:03 -0300

    Add services getAll members

--- services/members.js
+++ services/members.js
@@ -3,7 +3,10 @@ const membersRepository = require('../repositories/members');
 const remove = async (id) => {
   await membersRepository.remove(id);
 };
-
+const getAll = async () => {
+  return await membersRepository.getAll()
+}
 module.exports = {
-  remove
+  remove,
+  getAll
 };
