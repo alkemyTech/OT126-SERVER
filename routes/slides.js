281985f8c17276c1ea89717cad60467dbcdcc34d
Gimena Rivoira <73909780+GimenaRivoira@users.noreply.github.com>
2022-01-21 18:01:13 -0300

    Revert "getAll list for slides completed and add archive and commented lines that were causing problems to open the project (#10)" (#11)
    This reverts commit d4d4f7b0681bf32dec6f0ce5ff0502d4165abc0e.

--- routes/slides.js
+++ routes/slides.js
@@ -1,10 +1,9 @@
-const express = require('express')
-const slidesController = require('../controllers/slides')
-const authMiddleware = require('../middlewares/auth')
+const express = require('express');
+const slidesController = require('../controllers/slides');
+const authMiddleware = require('../middlewares/auth');
 
-const router = express.Router()
+const router = express.Router();
 
-router.delete('/:id', authMiddleware.isAdmin, slidesController.remove)
-router.get('/', slidesController.getAll)
+router.delete('/:id', authMiddleware.isAdmin, slidesController.remove);
 
-module.exports = router
+module.exports = router;
