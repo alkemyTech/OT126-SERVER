95c8a788518ad82808a5c168420693ae9f6eb7f9
gonzalo-m-ortiz <ogonzalo1234@gmail.com>
2022-01-21 09:55:12 -0300

    fix: validations improved + minor refactor

--- middlewares/news-validations.js
+++ middlewares/news-validations.js
@@ -1,16 +1,26 @@
 const { body } = require('express-validator')
 const { executeValidation } = require('./validation-index')
 
+// Common params
+
+const name = body('name')
+  .isString().withMessage('must be a string')
+  .notEmpty().withMessage('cannot be empty')
+
+const content = body('content')
+  .isString().withMessage('must be a string')
+  .notEmpty().withMessage('cannot be empty')
+
+const image = body('image')
+  .isString().withMessage('must be a string')
+  .isLength({ min: 1, max: 1234 }).withMessage('length range: 1-1234')
+
+// Validations
+
 const create = [
-  body('name')
-    .exists().withMessage('name required')
-    .isString().withMessage('name must be a string'),
-  body('content')
-    .exists().withMessage('content required')
-    .isString().withMessage('content must be a string'),
-  body('image')
-    .exists().withMessage('image required')
-    .isString().withMessage('image must be a string'),
+  name.exists().withMessage('param required'),
+  content.exists().withMessage('param required'),
+  image.exists().withMessage('param required'),
   executeValidation
 ]
 
--- services/news.js
+++ services/news.js
@@ -1,12 +1,12 @@
 const newsRepository = require('../repositories/news')
-const categoryRepository = require('../repositories/categories')
+const categoriesRepository = require('../repositories/categories')
 
 const create = async (noveltyToCreate) => {
-  const newsCategory = await categoryRepository.findByName('news')
+  const newsCategory = await categoriesRepository.findByName('news')
   if (newsCategory && newsCategory.id) {
     noveltyToCreate.categoryId = newsCategory.id
   } else {
-    // @TO-DO LOG TO DEVS. category 'news' not found
+    // @TODO LOG TO DEVS. category 'news' not found
     noveltyToCreate.categoryId = null
   }
   const noveltyCreated = await newsRepository.create(noveltyToCreate)
