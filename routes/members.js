dc194f03acb90039d2f2d436349364ddefe4b99e
Manu Ominuka <ominuka.mauro@gmail.com>
2022-01-21 11:55:35 -0300

    Fix: change middleware for authorization auth user

--- routes/members.js
+++ routes/members.js
@@ -5,7 +5,7 @@ const membersController = require('../controllers/members')
 const authMiddleware = require('../middlewares/auth')
 const { createReqValidation } = require('../middlewares/members-validations')
 
-router.post('/', authMiddleware.isAdmin, createReqValidation, membersController.create)
+router.post('/', authMiddleware.isAuth, createReqValidation, membersController.create)
 router.delete('/:id', authMiddleware.isAuth, membersController.remove)
 
 module.exports = router
