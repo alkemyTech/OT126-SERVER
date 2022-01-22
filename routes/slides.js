3d1c002ed194869b97a75ebe8996085246c685ba
GimenaRivoira <gimenarivoira@gmail.com>
2022-01-21 21:15:50 -0300

    validation for create in Slide working

--- routes/slides.js
+++ routes/slides.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const slidesController = require('../controllers/slides')
 const authMiddleware = require('../middlewares/auth')
+const slideValidations = require('../middlewares/slide-validations')
 
 const router = express.Router()
 
 router.delete('/:id', authMiddleware.isAdmin, slidesController.remove)
 router.get('/', slidesController.getAll)
 router.get('/:id', slidesController.getById)
-router.post('/create', slidesController.create)
+router.post('/create', slideValidations, slidesController.create)
 
 module.exports = router
