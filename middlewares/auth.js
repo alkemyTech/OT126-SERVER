ffaedfbe1d8a4a448f074405d8baa4db069e7d1b
Eduardo Corgniali <educorgniali@gmail.com>
2022-01-24 20:53:42 -0300

    added isAdmin middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,6 +1,10 @@
 const isAdmin = async (req, res, next) => {
-  // not implemented
-  next()
+  const { roleId } = req.user
+  if (roleId === 2) {
+    next()
+  } else {
+    res.status(403).json({ error: 'Unauthorized access' })
+  }
 }
 
 const isAuth = async (req, res, next) => {
