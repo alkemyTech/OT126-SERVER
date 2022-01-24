0bad30bbeeddc86d7ffdd3c5ed1ef00448493853
gonzalo-m-ortiz <51339776+gonzalo-m-ortiz@users.noreply.github.com>
2022-01-24 11:40:19 -0300

    Ot126 36 (#6)
    Fix:
    * News Model changed
    
    Features:
    * Create News, Endpoint
      "create" functions in:
        - news Routes
        - news Validations
        - news Controller
        - news Service
        - news Repository

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,7 @@ const organizationRoutes = require('./organizations')
 const membersRoute = require('./members')
 const commentsRoutes = require('./comments')
 const slidesRoute = require('./slides')
+const newsRoutes = require('./news')
 
 router.use('/categories', categoriesRoutes)
 router.use('/organizations', organizationRoutes)
@@ -15,5 +16,6 @@ router.use('/testimonials', testimonialsRoutes)
 router.use('/members', membersRoute)
 router.use('/comments', commentsRoutes)
 router.use('/slides', slidesRoute)
+router.use('/news', newsRoutes)
 
 module.exports = router
