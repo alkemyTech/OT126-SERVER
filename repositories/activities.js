fa314a62c0d869810893e0954d780b4b951df2d1
Eduardo Corgniali <educorgniali@gmail.com>
2022-01-24 11:21:19 -0300

    ffix repository layer variable declaration error

--- repositories/activities.js
+++ repositories/activities.js
@@ -1,7 +1,7 @@
 const db = require('../models')
 
 const create = async (data) => {
-  createdData = await db.Activities.create(data)
+  const createdData = await db.Activities.create(data)
   return {
     msg: 'Activity created successfully',
     data: createdData
