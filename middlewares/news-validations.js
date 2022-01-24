983ee5f33ba66c5d6a62f42724b1ec93bfceba06
gonzalo-m-ortiz <ogonzalo1234@gmail.com>
2022-01-24 12:06:04 -0300

    fix: update validation error

--- middlewares/news-validations.js
+++ middlewares/news-validations.js
@@ -6,31 +6,27 @@ const { executeValidation } = require('./validation-index')
 const name = body('name')
   .isString().withMessage('must be a string')
   .notEmpty().withMessage('cannot be empty')
+  .exists().withMessage('param required')
 
 const content = body('content')
   .isString().withMessage('must be a string')
   .notEmpty().withMessage('cannot be empty')
+  .exists().withMessage('param required')
 
 const image = body('image')
   .isString().withMessage('must be a string')
   .isLength({ min: 1, max: 1234 }).withMessage('length range: 1-1234')
+  .exists().withMessage('param required')
 
 const idParam = param('id')
   .isInt().withMessage('must be an integer')
+  .exists().withMessage('param required')
 
 // Validations
 
-const create = [
-  name.exists().withMessage('param required'),
-  content.exists().withMessage('param required'),
-  image.exists().withMessage('param required'),
-  executeValidation
-]
+const create = [name, content, image, executeValidation]
 
-const remove = [
-  idParam.exists().withMessage('param required'),
-  executeValidation
-]
+const remove = [idParam, executeValidation]
 
 module.exports = {
   create,
