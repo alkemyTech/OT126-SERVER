1045511033eb63d223b3bdf8919cf24ccf4cd380
Manu Ominuka <ominuka.mauro@gmail.com>
2022-01-24 08:16:20 -0300

    Add route for update member with id

--- routes/members.js
+++ routes/members.js
@@ -5,5 +5,6 @@ const membersController = require('../controllers/members');
 const authMiddleware = require('../middlewares/auth');
 
 router.delete('/:id', authMiddleware.isAuth, membersController.remove);
+router.put('/:id', authMiddleware.isAuth, membersController.update);
 
 module.exports = router;
