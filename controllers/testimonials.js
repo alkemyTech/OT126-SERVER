4f20dad73aafe175d7ce993939150f58e00d17cb
Victor Sanabria <61129336+Vungyr@users.noreply.github.com>
2022-01-25 09:31:54 -0300

    fix testimonial create service

--- controllers/testimonials.js
+++ controllers/testimonials.js
@@ -11,8 +11,8 @@ const remove = async (req, res, next) => {
 
 const create = async (req, res, next) => {
   try {
-    await testimonialsService.create(req.body)
-    res.status(200).json({ msg: 'Testimonial created succesfully', data: req.body })
+    const data = await testimonialsService.create(req.body)
+    res.status(200).json({ msg: 'Testimonial created succesfully', data: data })
   } catch (e) {
     next(e)
   }
