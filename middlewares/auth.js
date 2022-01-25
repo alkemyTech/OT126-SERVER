3348232869d3920f4f11ce63c73623d0b2a984a0
valentarayre <valentintarayre@gmail.com>
2022-01-25 17:48:27 -0300

    add isOwnUser

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,3 +1,6 @@
+const userRepository = require('../repositories/users')
+const { verifyToken } = require('../modules/auth')
+
 const isAdmin = async (req, res, next) => {
   // not implemented
   next()
@@ -9,8 +12,28 @@ const isAuth = async (req, res, next) => {
 }
 
 const isOwnUser = async (req, res, next) => {
-  // not implemented
-  next()
+  try {
+    const idParm = req.params.id    
+    const payload = getTokenPayload(req)
+    const user = await userRepository.getById(payload.userId)
+
+    if (!user) {
+      const error = new Error('Not found user with token provided.')
+      error.status = 403
+      throw error
+    }
+
+    if(payload.userId == idParm) return next()
+
+    if(user.roleID === 2) return next()
+
+    const error = new Error('Its not authorized')
+    error.status = 403
+    throw error
+
+  } catch (error) {
+    next(error)
+  }
 }
 
 module.exports = {
