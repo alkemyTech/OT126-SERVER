0397d0aa210cbed91940c02bd8ab5d562aa709c9
Manu Ominuka <ominuka.mauro@gmail.com>
2022-01-25 11:08:52 -0300

    Refactor method isAuth and add import userRepository

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,5 +1,6 @@
-const jwt = require('jsonwebtoken')
 const userRepository = require('../repositories/users')
+const { verifyToken } = require('../modules/auth')
+
 const isAdmin = async (req, res, next) => {
   // not implemented
   next()
@@ -7,20 +8,8 @@ const isAdmin = async (req, res, next) => {
 
 const isAuth = async (req, res, next) => {
   try {
-    const token = getToken(req)
-    if (!token) {
-      const error = new Error('Please provided a token Bearer in authorization.')
-      error.status = 403
-      throw error
-    }
-    const decodedToken = verifyToken(token)
-    if (!decodedToken) {
-      const error = new Error('The token provided isn\'t valid.')
-      error.status = 403
-      throw error
-    }
-    const user = await userRepository.getById(decodedToken.userId)
-    if (!user) {
+    const token = getTokenPayload(req)
+    if (!await userRepository.getById(token.userId)) {
       const error = new Error('Not found user with token provided.')
       error.status = 403
       throw error
@@ -36,15 +25,6 @@ const isOwnUser = async (req, res, next) => {
   next()
 }
 
-const getToken = async (req) => {
-  const authToken = req.headers.authorization
-  // authHeader && authHeader.split(' ')[1]
-  const token = authToken && authToken.startsWith('Bearer ').split(' ')[1]
-  return token
-}
-const verifyToken = async (token) => {
-  return jwt.verify(token, process.env.PASS_JWT)
-}
 module.exports = {
   isAdmin,
   isAuth,
