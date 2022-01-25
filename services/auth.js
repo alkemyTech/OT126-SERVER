a4056ff9f41a9ee77fbda416d939d9e7e92365f0
GimenaRivoira <gimenarivoira@gmail.com>
2022-01-25 20:03:21 -0300

    uncommenting lines

--- services/auth.js
+++ services/auth.js
@@ -1,5 +1,5 @@
 const bcrypt = require('bcrypt')
-/* const authModule = require('../modules/auth') */
+const authModule = require('../modules/auth')
 const usersRepository = require('../repositories/users')
 
 const login = async (credentials) => {
@@ -17,18 +17,18 @@ const login = async (credentials) => {
     throw error
   } // code waiting for an error handler
 
-/*   const token = getToken(user)
-  return token */
+  const token = getToken(user)
+  return token
 }
 
 module.exports = {
   login
 }
 
-/* function getToken (user) {
+function getToken (user) {
   const tokenPayload = {
     userId: user.id
   }
   const token = authModule.createToken(tokenPayload)
   return token
-} */
+}
