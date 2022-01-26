cf200bb6637acd7c30e9a0988d0e36f5072c1e4b
Matias <matiasmarin45@gmail.com>
2022-01-26 08:59:21 -0300

    Fixing conflict

--- routes/auth.js
+++ routes/auth.js
@@ -5,12 +5,8 @@ const router = express.Router()
 const authController = require('../controllers/auth')
 const authValidations = require('../middlewares/auth-validations')
 
-<<<<<<< .merge_file_a02964
-router.post('/login', authValidations.login, authController.login)
-=======
 router.get('/', authController.getAll)
 router.post('/login', authValidations.login, authController.login)
 router.post('/register', authController.create)
->>>>>>> .merge_file_a03352
 
 module.exports = router
--- routes/members.js
+++ routes/members.js
@@ -1,6 +1,5 @@
 const express = require('express')
 
-<<<<<<< .merge_file_a12744
 const router = express.Router()
 const membersController = require('../controllers/members')
 const authMiddleware = require('../middlewares/auth')
@@ -9,18 +8,6 @@ const { updateReqValidation, createReqValidation } = require('../middlewares/mem
 router.delete('/:id', authMiddleware.isAuth, membersController.remove)
 router.put('/:id', authMiddleware.isAuth, updateReqValidation, membersController.update)
 router.post('/', authMiddleware.isAuth, createReqValidation, membersController.create)
-router.delete('/:id', authMiddleware.isAuth, membersController.remove)
 router.get('/', membersController.getAll)
-=======
-const router = express.Router();
-const membersController = require('../controllers/members');
-const authMiddleware = require('../middlewares/auth');
-const { updateReqValidation, createReqValidation } = require('../middlewares/members-validations')
-
-router.delete('/:id', authMiddleware.isAuth, membersController.remove);
-router.put('/:id', authMiddleware.isAuth, updateReqValidation, membersController.update);
-router.post('/', authMiddleware.isAuth, createReqValidation, membersController.create)
-router.get('/', membersController.getAll);
->>>>>>> .merge_file_a04844
 
 module.exports = router
