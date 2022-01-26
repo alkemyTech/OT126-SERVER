e188df0a686a2525209ba65b0c10c6d4daa4fe1b
Ezequiel Bauer <83975962+EzeBauer@users.noreply.github.com>
2022-01-25 21:50:38 -0300

    Ot126 61 (#21)
    * changes to start project
    
    * Obtener lista de comentarios
    
    * Create connents and validations finished
    
    * delete package-lock.json
    
    * errores marcados solucionados

--- models/comments.js
+++ models/comments.js
@@ -8,6 +8,8 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate (models) {
+      Comments.belongsTo(models.Users)
+      Comments.belongsTo(models.News)
     }
   }
   Comments.init(
--- services/comments.js
+++ services/comments.js
@@ -1,9 +1,24 @@
 const commentsRepository = require('../repositories/comments')
 
-const remove = async (id) => {
-  await commentsRepository.remove(id)
+const getAll = async () => {
+  const response = await commentsRepository.getAll()
+  return response
 }
+const create = async (data) => {
+  const response = await commentsRepository.create(data)
+
+  if (!response) {
+    const error = new Error('there was an error in comment creation')
+    error.status = 403
+    throw error
+  }
+  return response
+}
+/* const remove = async (id) => {
+  await commentsRepository.remove(id);
+}; */
 
 module.exports = {
-  remove
+  getAll,
+  create
 }
