614f2a0f1ddbba65c7db9ecfd53d679d85b2b2e1
Victor Sanabria <61129336+Vungyr@users.noreply.github.com>
2022-01-26 09:57:14 -0300

    Ot126 55 - Testimonial update feature (#16)
    * added testimonial update feature
    
    * added testimonial update feature
    
    * fix return data from db and move validation to service

--- controllers/testimonials.js
+++ controllers/testimonials.js
@@ -9,6 +9,16 @@ const remove = async (req, res, next) => {
   }
 }
 
+const update = async (req, res, next) => {
+  try {
+    const testimonialUpdated = await testimonialsService.update(req.params.id, req.body);
+    res.status(200).json({ msg: `Testimonial ${testimonialUpdated.id} updated succesfully`, data: testimonialUpdated });
+  } catch (error) {
+    next (error)
+  }
+  
+}
+
 const getAll = async (req, res, next) => {
   try {
     const testimonials = await testimonialsService.getAll()
@@ -19,6 +29,7 @@ const getAll = async (req, res, next) => {
 }
 
 module.exports = {
-  remove,
+  update,
+  remove
   getAll,
 }
