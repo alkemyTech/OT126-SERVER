81a8fd505d76143fdbec046f95557cea332ffb65
gonzalo-m-ortiz <ogonzalo1234@gmail.com>
2022-01-27 10:34:29 -0300

    fix: error handler now returning json

--- app.js
+++ app.js
@@ -29,14 +29,18 @@ app.use((req, res, next) => {
 })
 
 // error handler
-app.use((err, req, res) => {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development' ? err : {}
 
-  // render the error page
-  res.status(err.status || 500)
-  res.render('error')
+  // normalize error
+  if (!err.status) {
+    err.status = 500
+    err.message = 'Internal Server Error'
+  }
+
+  res.status(err.status).json({ error: err.message })
 })
 
 module.exports = app
