6f9c33b5808d7b9bb6c50fb36d4e554732df2ca9
valentarayre <valentintarayre@gmail.com>
2022-01-28 07:39:08 -0300

    fix url

--- modules/pagination.js
+++ modules/pagination.js
@@ -1,31 +1,38 @@
 
-const getPreviusPage = (count, offset) => {
-  const currentPage = Math.round(offset / 10) + 1
-  if (count > offset) return offset === 0 ? null : currentPage - 1
-  return Math.ceil(count / 10)
-}
-
-const getNextPage = (count, offset) => {
-  const totalPages = Math.ceil(count / 10)
-  const currentPage = Math.ceil(offset / 10) + 1
-  return (totalPages - currentPage) > 0
-    ? parseInt(currentPage) + 1
-    : null
-}
-
 const getOffset = (page) => {
   let defaultValue = 0
   if (page && page !== '1' && page !== '0') defaultValue = Math.abs(10 * page) - 10
   return isNaN(defaultValue) ? 0 : defaultValue
 }
 
+const getUrl = (req) => {
+  const { protocol, host, baseUrl } = req
+  const port = process.env.PORT || '3000'
+  return `${protocol}://${host}:${port}${baseUrl}/?page=`
+}
+
+const getPreviusPage = (count, offset, url) => {
+  const currentPage = Math.round(offset / 10) + 1
+  if (count > offset) return offset === 0 ? null : `${url}${currentPage - 1}`
+  return Math.ceil(count / 10)
+}
+
+const getNextPage = (count, offset, url) => {
+  const totalPages = Math.ceil(count / 10)
+  const currentPage = Math.ceil(offset / 10) + 1
+  return (totalPages - currentPage) > 0
+    ? `${url}${parseInt(currentPage) + 1}`
+    : null
+}
+
 const pagination = async (repositorie, req) => {
   const { page } = req.query
   const offset = getOffset(page)
   const data = await repositorie(offset)
+  const url = getUrl(req)
 
-  const previusPage = getPreviusPage(data.count, offset)
-  const nextPage = getNextPage(data.count, offset)
+  const previusPage = getPreviusPage(data.count, offset, url)
+  const nextPage = getNextPage(data.count, offset, url)
 
   return {
     previusPage,
