318c97dbd5c5b3ad078839cae810c237ed51cbff
Manu Ominuka <ominuka.mauro@gmail.com>
2022-01-28 09:59:18 -0300

    Restore comment in modules/auth

--- modules/auth.js
+++ modules/auth.js
@@ -5,6 +5,8 @@ function createToken (payload) {
   return jwt.sign(payload, token.secret, { expiresIn: token.expiresIn })
 }
 
+/* verifyToken return a payload object with userId,
+    creation time "iat" and expiry time "exp" */
 function verifyToken (tokenUser) {
   const payload = jwt.verify(tokenUser, token.secret)
   return payload
