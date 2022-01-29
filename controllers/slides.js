b5c1821273065266023742071e0ddb2e19a43a61
GimenaRivoira <gimenarivoira@gmail.com>
2022-01-29 16:46:58 -0300

    added message in json

--- controllers/slides.js
+++ controllers/slides.js
@@ -33,7 +33,7 @@ const create = async (req, res, next) => {
   try {
     const data = await slidesService.create(req.body)
 
-    res.status(200).json({ data: data })
+    res.status(200).json({ msg: 'Slide successfully created', data: data })
   } catch (error) {
     next(error)
   }
