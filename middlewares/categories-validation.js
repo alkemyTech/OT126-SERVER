44deeeb0a4d4dca0c3784b9b91c0bcc3fcaee373
valentarayre <valentintarayre@gmail.com>
2022-01-31 00:40:24 -0300

    add bail for single error

--- middlewares/categories-validation.js
+++ middlewares/categories-validation.js
@@ -3,33 +3,33 @@ const { executeValidation } = require('./validation-index')
 
 exports.createValidationCategory = [
   body('name')
-    .exists()
-    .isString().withMessage('name has to be a string')
-    .isLength({ min: 1, max: 200 }).withMessage('must have content'),
+    .exists().withMessage('Name is required').bail()
+    .isString().withMessage('Name has to be a string').bail()
+    .isLength({ min: 1, max: 200 }).withMessage('Must have content').bail(),
   body('description')
     .optional()
-    .isString().withMessage('description has to be a string')
-    .isLength({ min: 1, max: 200 }).withMessage('must have content'),
+    .isString().withMessage('Description has to be a string').bail()
+    .isLength({ min: 1, max: 200 }).withMessage('Must have content').bail(),
   body('image')
     .optional()
-    .isString().withMessage('image has to be a string')
-    .isLength({ min: 1, max: 200 }).withMessage('must have content'),
+    .isString().withMessage('Image has to be a string').bail()
+    .isLength({ min: 1, max: 200 }).withMessage('Must have content').bail(),
   executeValidation
 ]
 exports.updateValidationCategory = [
   param('id')
-    .isInt().withMessage('id has to be a number'),
+    .isInt().withMessage('id has to be a number').bail(),
   body('name')
     .optional()
-    .isString().withMessage('name has to be a string')
-    .isLength({ min: 1, max: 200 }).withMessage('must have content'),
+    .isString().withMessage('name has to be a string').bail()
+    .isLength({ min: 1, max: 200 }).withMessage('must have content').bail(),
   body('description')
     .optional()
-    .isString().withMessage('description has to be a string')
-    .isLength({ min: 1, max: 200 }).withMessage('must have content'),
+    .isString().withMessage('description has to be a string').bail()
+    .isLength({ min: 1, max: 200 }).withMessage('must have content').bail(),
   body('image')
     .optional()
-    .isString().withMessage('image has to be a string')
-    .isLength({ min: 1, max: 200 }).withMessage('must have content'),
+    .isString().withMessage('image has to be a string').bail()
+    .isLength({ min: 1, max: 200 }).withMessage('must have content').bail(),
   executeValidation
 ]
