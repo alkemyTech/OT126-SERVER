dbdd827e4084612cca27517248da0e77c157ac15
Gimena Rivoira <73909780+GimenaRivoira@users.noreply.github.com>
2022-02-01 13:48:32 -0300

    refactoring of list of users (#49)

--- routes/auth.js
+++ routes/auth.js
@@ -4,8 +4,9 @@ const router = express.Router()
 
 const authController = require('../controllers/auth')
 const authValidations = require('../middlewares/auth-validations')
+const authMiddleware = require('../middlewares/auth')
 
-router.get('/', authController.getAll)
+router.get('/', authMiddleware.isAdmin, authController.getAll)
 router.post('/login', authValidations.login, authController.login)
 router.post('/register', authValidations.register, authController.create)
 
