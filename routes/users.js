ac9f91128614dabb55c1dc74225ab9a1855acd63
vungyr <viktor-1051@hotmail.com>
2022-02-01 09:53:44 -0300

    change PUT method to PATCH

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,6 @@ const userValidations = require('../middlewares/users-validation');
 const authMiddleware = require('../middlewares/auth')
 const usersController = require('../controllers/users')
 
-router.put('/:id' ,userValidations.userUpdateValidation , authMiddleware.isAuth, usersController.update)
+router.patch('/:id' ,userValidations.userUpdateValidation , authMiddleware.isAuth, usersController.update)
 
 module.exports = router
