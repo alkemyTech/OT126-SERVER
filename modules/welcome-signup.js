5431c98cb46d9168ffd9583d8efe4ed7832cf564
Manu Ominuka <ominuka.mauro@gmail.com>
2022-02-01 09:36:33 -0300

    Refactor fixes

--- modules/welcome-signup.js
+++ modules/welcome-signup.js
@@ -3,7 +3,15 @@ const ejs = require('ejs')
 const path = require('path')
 
 const createTemplateWelcome = async (organization) => {
-  return await ejs.renderFile(path.join(process.cwd(), 'templates', 'signup-email.html'), organization)
+  const data = {
+    name: organization.name,
+    image: organization.image,
+    address: organization.address,
+    phone: organization.phone,
+    welcomeText: organization.welcomeText,
+    email: organization.email
+  }
+  return await ejs.renderFile(path.join(process.cwd(), 'templates', 'signup-email.ejs'), data).then(data => data)
 }
 
 module.exports = {
