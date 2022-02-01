44183a802ead1ab9e942a1cb5cbb5c42f5b9a2e1
Ominuka Mauro <64550079+OmiMauro@users.noreply.github.com>
2022-02-01 20:41:31 -0300

    OT126-24 - Remove a User by ID (#69)
    - Add router for delete a user by ID and if isAuth and isOwnUser
    - Add controller for remove a user
    - Add remove in layer services
    - Refactor method remove from repository

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,12 @@
-const express = require('express');
-const router = express.Router();
-const userValidations = require('../middlewares/users-validation');
+const express = require('express')
+const router = express.Router()
+const userValidations = require('../middlewares/users-validation')
 
 const authMiddleware = require('../middlewares/auth')
 const usersController = require('../controllers/users')
 
+router.put('/:id', userValidations.userUpdateValidation, authMiddleware.isAuth, usersController.update)
+router.delete('/:id', authMiddleware.isAuth, authMiddleware.isOwnUser, usersController.remove)
 router.patch('/:id' ,userValidations.userUpdateValidation , authMiddleware.isAuth, usersController.update)
 
 module.exports = router
