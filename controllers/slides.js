84dfd382b4a71ce6254ba1c557247dfcfb2a5bc9
Gimena Rivoira <73909780+GimenaRivoira@users.noreply.github.com>
2022-02-01 10:23:52 -0300

    OT126-46-newFixes (#56)
    * added create in controller that was missing
    
    * added message in json

--- controllers/slides.js
+++ controllers/slides.js
@@ -29,8 +29,19 @@ const getById = async (req, res, next) => {
   }
 }
 
+const create = async (req, res, next) => {
+  try {
+    const data = await slidesService.create(req.body)
+
+    res.status(200).json({ msg: 'Slide successfully created', data: data })
+  } catch (error) {
+    next(error)
+  }
+}
+
 module.exports = {
   remove,
   getAll,
-  getById
+  getById,
+  create
 }
