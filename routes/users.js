df6b0bcf5f13a9a12f9802b6300ac83c46879798
Manu Ominuka <ominuka.mauro@gmail.com>
2022-02-01 09:43:21 -0300

    Add router for delete a user

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const router = express.Router();
-const userValidations = require('../middlewares/users-validation');
+const express = require('express')
+const router = express.Router()
+const userValidations = require('../middlewares/users-validation')
 
 const authMiddleware = require('../middlewares/auth')
 const usersController = require('../controllers/users')
 
-router.put('/:id' ,userValidations.userUpdateValidation , authMiddleware.isAuth, usersController.update)
-
+router.put('/:id', userValidations.userUpdateValidation, authMiddleware.isAuth, usersController.update)
+router.delete('/:id', authMiddleware.isAuth, authMiddleware.isOwnUser, usersController.remove)
 module.exports = router
