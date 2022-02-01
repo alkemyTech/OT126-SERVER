baec9cb8b484575022605ede61c248d6c8d74fb5
valentarayre <87746742+valentarayre@users.noreply.github.com>
2022-02-01 20:09:34 -0300

    Ot126-28 Create auth/me (#63)
    * add function getByToken
    
    * add new router for /auth/me

--- routes/auth.js
+++ routes/auth.js
@@ -6,8 +6,10 @@ const authController = require('../controllers/auth')
 const authValidations = require('../middlewares/auth-validations')
 const authMiddleware = require('../middlewares/auth')
 
+
 router.get('/', authMiddleware.isAdmin, authController.getAll)
 router.post('/login', authValidations.login, authController.login)
 router.post('/register', authValidations.register, authController.create)
+router.get('/me', authMiddlewares.isAuth, authController.getByToken)
 
 module.exports = router
