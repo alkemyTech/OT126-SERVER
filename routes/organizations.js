7fec654cd0bf9bd45deedf326b65e84443c43d9f
GimenaRivoira <gimenarivoira@gmail.com>
2022-02-01 22:32:53 -0300

    delete route that was added

--- routes/organizations.js
+++ routes/organizations.js
@@ -5,7 +5,6 @@ const router = express.Router()
 const organizationController = require('../controllers/organizations')
 const authMiddleware = require('../middlewares/auth')
 
-router.get('/public', organizationController.getAll)
 router.put('/public', authMiddleware.isAdmin, organizationController.update)
 router.get('/public', organizationController.getById)
 
