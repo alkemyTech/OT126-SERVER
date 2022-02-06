9cf971ec046777888672f314b627346a787fee62
Manu Ominuka <ominuka.mauro@gmail.com>
2022-02-06 15:31:12 -0300

    Changes validations from check to body

--- middlewares/members-validations.js
+++ middlewares/members-validations.js
@@ -1,13 +1,13 @@
 
-const { check, param } = require('express-validator')
+const { body, param } = require('express-validator')
 
 const { executeValidation } = require('./validation-index')
 
-const name = check('name', 'Must be input a name valid')
+const name = body('name', 'Must be input a name valid')
   .notEmpty()
   .trim()
 
-const image = check('image')
+const image = body('image')
   .if((value, { req }) => !req.file)
   .isString().withMessage('Must be a string').bail()
   .isLength({ min: 1, max: 1234 }).withMessage('length range: 1-1234').bail()
