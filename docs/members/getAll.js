1545de8a44bd79cbbe70b7d3c0c5dabcfa9944ae
Manu Ominuka <ominuka.mauro@gmail.com>
2022-02-07 21:58:23 -0300

    Refactor method getAll and added two schema how response

--- docs/members/getAll.js
+++ docs/members/getAll.js
@@ -8,22 +8,69 @@ module.exports = {
     parameters: [
       {
         name: 'page',
-        type: 'integer',
-        in: 'path',
-        description: 'Number of page to find. Default 1. Integer',
-        summary: '10 members by page',
-        required: false
+        in: 'query',
+        schema: {
+          type: 'number',
+          example: 1
+        },
+        description: 'Number of the page'
       }
     ],
     responses: {
       200: {
-        description: 'Get all members',
+        description: 'List of news with pagination data (Multiple schemas)',
         content: {
           'application/json': {
             schema: {
-              type: 'array',
-              items: {
-                schema: memberSchema.Members
+              type: 'object',
+              properties: {
+                pagesUrl: {
+                  type: 'object',
+                  properties: {
+                    previous: {
+                      oneOf: [
+                        {
+                          type: 'string',
+                          description: 'url to previous page',
+                          example: 'http://localhost:3000/news/?page=1'
+                        },
+                        {
+                          type: 'null',
+                          description: 'previous page does not exist',
+                          example: null
+                        }
+                      ]
+                    },
+                    next: {
+                      oneOf: [
+                        {
+                          type: 'null',
+                          description: 'next page does not exist',
+                          example: null
+                        },
+                        {
+                          type: 'string',
+                          description: 'url to next page',
+                          example: 'http://localhost:3000/news/?page=3'
+                        }
+                      ]
+                    }
+                  }
+                },
+                itemsCount: {
+                  type: 'number',
+                  description: 'count of entities in database',
+                  example: 15
+                },
+                totalPages: {
+                  type: 'number',
+                  description: 'count of available pages',
+                  example: 2
+                },
+                data: {
+                  type: 'array',
+                  items: memberSchema.Members
+                }
               }
             }
           }
