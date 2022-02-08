75b92e9ca4480955e45b51c5999dac073e8442fb
gonzalo-m-ortiz <ogonzalo1234@gmail.com>
2022-02-07 23:21:41 -0300

    news docs added to index

--- docs/news/index.js
+++ docs/news/index.js
@@ -1,9 +1,18 @@
+const create = require('./create')
+const update = require('./update')
+const remove = require('./remove')
+const getAll = require('./getAll')
+const getById = require('./getById')
+
 module.exports = {
   '/news': {
-
+    ...create,
+    ...getAll
   },
   '/news/{id}': {
-
+    ...update,
+    ...remove,
+    ...getById
   },
   '/news/{id}/comments': {
 
