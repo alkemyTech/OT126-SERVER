70429e88673931ba4e9315add8f65aa993553c9a
Manu Ominuka <ominuka.mauro@gmail.com>
2022-02-08 11:35:16 -0300

    Remove import schema in the root of doc

--- docs/index.js
+++ docs/index.js
@@ -4,15 +4,14 @@ const components = require('./components')
 const categories = require('./categories/index')
 const servers = require('./servers')
 
-const members = require('./members/index')
 module.exports = {
   ...basicInfo,
   ...servers,
   ...tags,
   ...components,
   paths: {
-    ...categories,
-    ...members
+    ...categories
+
   }
 
 }
