21d8c2c265212ac42e5a424b069efe24409107ca
Gimena Rivoira <73909780+GimenaRivoira@users.noreply.github.com>
2022-02-11 18:33:55 -0300

    added images module in slide (#89)
    * added images modules in slide
    
    * added isString validation
    
    * fix gramatical errors

--- middlewares/slide-validations.js
+++ middlewares/slide-validations.js
@@ -1,20 +1,23 @@
-const { check } = require('express-validator')
+const { check, body } = require('express-validator')
 const { executeValidation } = require('./validation-index')
 
+const image = body('imageUrl')
+  .if((value, { req }) => !req.file)
+  .isString().withMessage('must be a string').bail().isLength({ min: 1, max: 1234 }).withMessage('length range: 1-1234')
+
 const createSlide = [
-  check('imageUrl').notEmpty().matches(/^[A-Z]+$/i).withMessage('This camp is required and has to be a string'),
-  check('text').notEmpty().matches(/^[A-Z]+$/i).withMessage('This camp is required and has to be a string'),
+  image,
+  check('text').notEmpty().withMessage('This camp is required and has to be a string'),
   check('order').notEmpty().isNumeric().withMessage('This camp is required and has to be a number'),
   check('organizationId').notEmpty().isNumeric().withMessage('This camp is required and has to be a number'),
   executeValidation
 ]
 
 const updateSlide = [
-  check('imageUrl').matches(/^[A-Z]+$/i).optional().withMessage('This camp has to be a string'),
-  check('text').matches(/^[A-Z]+$/i).optional().withMessage('This camp has to be a string'),
+  check('text').isString().optional().withMessage('This camp has to be a string'),
   check('order').isNumeric().optional().withMessage('This camp has to be a number'),
   check('organizationId').isNumeric().optional().withMessage('This camp has to be a number'),
-  executeValidation
+  image, executeValidation
 ]
 
 module.exports = {
